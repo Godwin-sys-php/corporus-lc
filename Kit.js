3a50f35d4b3695461fc0852741dca64035c86abf
Godwin Burume <Godwin Burume>
2024-12-01 19:18:48 +0100

    inventaire 1/12

--- Kit.js
+++ Kit.js
@@ -1,23 +1,20 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-const sqlFile = fs.createWriteStream('kit.sql');
+const sqlFile = fs.createWriteStream('new.sql');
 
-fs.createReadStream('plats_7avr.csv')
+fs.createReadStream('Classeur1.csv')
   .pipe(csv({separator: ';'}))
   .on('data', (row) => {
     // Créer la requête INSERT pour chaque ligne
-      if (Number(row.reel) > 0) {
-        const insertQuery = `UPDATE products SET inStock = ${Number(row.reel)} where id=${Number(row.id)};\n`;
-        
-        // Écrire la requête dans le fichier SQL
-        sqlFile.write(insertQuery);
-      } else {
-        const insertQuery = `UPDATE products SET inStock = 0 where id=${Number(row.id)};\n`;
-        
-        // Écrire la requête dans le fichier SQL
-        sqlFile.write(insertQuery);
-      }
+    const namef = Object.values(row)[0];
+    const stock = Object.values(row)[2];
+    
+    const escapedName = namef.replace(/'/g, "''"); // Double l'apostrophe
+      const insertQuery = `UPDATE products SET inStock = ${stock} where name='${(escapedName)}';\n`;
+      
+      // Écrire la requête dans le fichier SQL
+      sqlFile.write(insertQuery);
   })
   .on('end', () => {
     console.log('CSV file successfully processed. SQL queries written to boissons.sql');
